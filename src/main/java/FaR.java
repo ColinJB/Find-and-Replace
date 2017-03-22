de53a8300201786ec973e6fd56ebbd77c3a10e93
Colin Bloom <colinjbloom113@gmail.com>
2017-03-22 16:39:57 -0700

    initial commit

--- src/main/java/FaR.java
+++ src/main/java/FaR.java
@@ -3,5 +3,11 @@ import java.util.ArrayList;
 
 
 public class FaR {
-  List<String> myStringList = new ArrayList<String>();
+  String[] input = {};
+
+  public String wordReplace(String phrase, String word, String replacementWord) {
+    return "";
+    // return {"a word", "word", "new"};
+  }
+
 }
--- src/test/java/FaRTest.java
+++ src/test/java/FaRTest.java
@@ -4,10 +4,17 @@ import static org.junit.Assert.*;
 public class FaRTest {
 
   @Test
-  public void newArray_instantiatesCorrectly_Array() {
+  public void wordReplace_instantiatesCorrectly_Array() {
     FaR testFar = new FaR();
     assertEquals(true, testFar instanceof FaR);
-
   }
+
+  @Test
+  public void wordReplace_returnWord_Strings() {
+    FaR testFar = new FaR();
+    String[] expected = {"a word", "word", "new"};
+    assertEquals(expected, testFar.wordReplace("","",""));
+  }
+
 
 }
