9d4042688bb9636672491a01a27c39ed719b9f9a
Colin Bloom <colinjbloom113@gmail.com>
2017-03-23 10:23:26 -0700

    final commit, add user interface

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,5 +1,17 @@
+import java.io.Console;
+
 public class App {
   public static void main(String[] args) {
-    System.out.println("Find and Replace!");
+    Console userConsole = System.console();
+    System.out.println("Welcome to Find and Replace! Please enter a sentence.");
+    String sentence = userConsole.readLine();
+    System.out.println("Please choose a word from that sentence which you would like to replace.");
+    String word = userConsole.readLine();
+    System.out.println("Please choose a word that you would like to replace the previous word with.");
+    String replacementWord = userConsole.readLine();
+    FaR far = new FaR();
+    String newPhrase = far.wordReplace(sentence, word, replacementWord);
+    System.out.println("Your new phrase is: " + newPhrase);
+
   }
 }
